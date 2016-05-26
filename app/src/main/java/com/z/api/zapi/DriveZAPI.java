ce2babd2f4994c5b9510017443b9f9a8b7ba1b27
Zacc <zacchari.emanuel@gmail.com>
2016-05-26 08:57:53 -0400

    Improved the Google Drive zApi

--- app/src/main/java/com/z/api/zapi/DriveZAPI.java
+++ app/src/main/java/com/z/api/zapi/DriveZAPI.java
@@ -1,93 +1,191 @@
 package com.z.api.zapi;
 
 import android.content.Context;
+import android.os.ParcelFileDescriptor;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.drive.Drive;
-import com.google.android.gms.drive.DriveApi;
 import com.google.android.gms.drive.DriveContents;
+import com.google.android.gms.drive.DriveFile;
 import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.DriveId;
 import com.google.android.gms.drive.MetadataChangeSet;
+import com.google.android.gms.drive.DriveApi.DriveContentsResult;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 
-public class DriveZAPI implements ZAPIClient.ZAPI{
+public class DriveZAPI implements ZAPIClient.ZAPI {
+    class GoogleDocument {
+            GoogleDocument(ZAPIClient client) {
+                this.client = client;
+            }
 
-    //Create a googleDocs text file on the user's drive account
-    public void CreateDocument(ZAPIClient client, String fileTitle, String fileContents, boolean isStarred){
-        if (client.getClient().isConnected()) {
-            Drive.DriveApi.newDriveContents(client.getClient())
-                    .setResultCallback(driveContentsCallback(client.getClient(), fileTitle, fileContents,isStarred));
-        } else {
-            client.getClient().connect();
-        }
-    }
+            public boolean hasLoaded = false;
+            private ZAPIClient client;
+            private DriveId driveId = null;
+            private DriveFile file;
+            String content;
 
-    //This function returns a new callback class.
-    //The new callback class essentially waits for your phone to request access from google docs.
-    //Specifically, the callback is waiting to see if you can create new "driveContents",
-    //imagine asking your computer if you could make a new .png file.
-    //One things to note is that it does not place the file on your drive account just yet,
-    //It is merely allowing you to generate and edit a file's data on your phone
-    //The fileCallback is what waits upon the actual addition of the file to your drive account
-    private ResultCallback<DriveApi.DriveContentsResult> driveContentsCallback(final GoogleApiClient client, final String sTitle, final String sFileText, final boolean isStarred) {
-        return new
-                ResultCallback<DriveApi.DriveContentsResult>() {
+            public void setID(String id){
+                driveId = DriveId.decodeFromString(id);
+            }
+            public String getID(){
+                return driveId.encodeToString();
+            }
+
+            public void LoadFromId(String id) {
+                setID(id);
+                Load();
+            }
+            public void Load() {
+                checkConnection();
+                hasLoaded = false;
+                new Thread() {
                     @Override
-                    public void onResult(DriveApi.DriveContentsResult result) {
-                        if (!result.getStatus().isSuccess()) {
-                            Toaster("Error while trying to create new file contents");
-                            return;
-                        }
-                        final DriveContents driveContents = result.getDriveContents();
+                    public void run() {
+                        file = driveId.asDriveFile();
+                        file.open(client.getClient(), DriveFile.MODE_READ_ONLY, null)
+                                .setResultCallback(new ResultCallback<DriveContentsResult>() {
+                                                       @Override
+                                                       public void onResult(@NonNull DriveContentsResult result) {
+                                                           if (!result.getStatus().isSuccess()) {
+                                                               onLoadFailure();
+                                                           } else {
+                                                               DriveContents contents = result.getDriveContents();
+                                                               BufferedReader reader = new BufferedReader(new InputStreamReader(contents.getInputStream()));
+                                                               StringBuilder builder = new StringBuilder();
+                                                               String line;
+                                                               try {
+                                                                   while ((line = reader.readLine()) != null) {
+                                                                       builder.append(line);
+                                                                   }
+                                                               } catch (Exception e) {
+                                                                   onLoadFailure();
+                                                               }
+                                                               content = builder.toString();
+                                                               contents.discard(client.getClient());
+                                                               onLoadSuccess(content);
+                                                               hasLoaded = true;
+                                                           }
+                                                       }
+                                                   }
+                                );
+                    }
+                }.start();
+            }
 
-                        // Perform I/O off the UI thread.
-                        new Thread() {
+            public void Write(String content){
+                this.content = content;
+                Write();
+            }
+            public void WriteToId(String id, String content){
+                this.content = content;
+                WriteToId(id);
+            }
+
+            public void Write(){
+                checkConnection();
+                if (driveId == null){
+                    Toaster("This file has no id! You must either write to an id, or call generateDocument()");
+                    return;
+                }
+                new Thread() {
+                    @Override
+                    public void run() {
+                        file = driveId.asDriveFile();
+                        file.open(client.getClient(), DriveFile.MODE_WRITE_ONLY, null).setResultCallback(new ResultCallback<DriveContentsResult>() {
                             @Override
-                            public void run() {
-                                // write content to DriveContents
-                                OutputStream outputStream = driveContents.getOutputStream();
-                                Writer writer = new OutputStreamWriter(outputStream);
-                                try {
-                                    writer.write(sFileText);
-                                    writer.close();
-                                } catch (IOException e) {
-                                    //Log.e(TAG, e.getMessage());
+                            public void onResult(DriveContentsResult result) {
+                                if (!result.getStatus().isSuccess()) {
+                                    onWriteFailure();
+                                    return;
                                 }
+                                DriveContents driveContents = result.getDriveContents();
 
-                                MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
-                                        .setTitle(sTitle)
-                                        .setMimeType("text/plain")
-                                        .setStarred(isStarred).build();
-
-                                // create a file in root folder
-                                Drive.DriveApi.getRootFolder(client)
-                                        .createFile(client, changeSet, driveContents)
-                                        .setResultCallback(fileCallback());
+                                try {
+                                    ParcelFileDescriptor parcelFileDescriptor = driveContents.getParcelFileDescriptor();
+                                    FileOutputStream fileOutputStream = new FileOutputStream(parcelFileDescriptor
+                                            .getFileDescriptor());
+                                    Writer writer = new OutputStreamWriter(fileOutputStream);
+                                    System.out.println(content);
+                                    writer.write(content);
+                                    writer.close();
+                                }catch(Exception e){
+                                    onWriteFailure();
+                                }
+                                driveContents.commit(client.getClient(), null).setResultCallback(new ResultCallback<Status>() {
+                                    @Override
+                                    public void onResult(Status result) {
+                                        if (!result.getStatus().isSuccess()){
+                                            onWriteFailure();
+                                        }
+                                        onWriteSuccess();
+                                    }
+                                });
                             }
-                        }.start();
+                        });
                     }
-                };
-    }
+                }.start();
+            }
 
-    //Handles errors that may occur during the creation of the file on google drive
-    private ResultCallback<DriveFolder.DriveFileResult> fileCallback() {
-        return new
-                ResultCallback<DriveFolder.DriveFileResult>() {
+            public void WriteToId(String id){
+                setID(id);
+                Write();
+            }
+
+            public void generateDocument(final String fileTitle, final boolean isStarred) {
+                new Thread() {
                     @Override
-                    public void onResult(DriveFolder.DriveFileResult result) {
-                        if (!result.getStatus().isSuccess()) {
-                            Toaster("Error while trying to create the file");
-                            return;
-                        }
-                        Toaster("Created a file with content at: " + result.getDriveFile().getDriveId());
+                    public void run() {
+                        Drive.DriveApi.newDriveContents(client.getClient())
+                                .setResultCallback(new ResultCallback<DriveContentsResult>() {
+                                    @Override
+                                    public void onResult(@NonNull DriveContentsResult driveContentsResult) {
+                                        Drive.DriveApi.getRootFolder(client.getClient())
+                                                .createFile(client.getClient(), new MetadataChangeSet.Builder()
+                                                        .setTitle(fileTitle)
+                                                        .setMimeType("text/plain")
+                                                        .setStarred(isStarred).build(), driveContentsResult.getDriveContents())
+                                                .setResultCallback(new ResultCallback<DriveFolder.DriveFileResult>() {
+                                                    @Override
+                                                    public void onResult(@NonNull DriveFolder.DriveFileResult driveFileResult) {
+                                                        setID(driveFileResult.getDriveFile().getDriveId().encodeToString());
+                                                        onGenerateSuccess(getID());
+                                                    }
+                                                });
+
+                                    }
+                                });
                     }
-                };
+                }.start();
+            }
+
+            public void onGenerateSuccess(String id) {}
+
+            public void onGenerateFailure() {}
+
+            public void onLoadSuccess(String content) {}
+
+            public void onLoadFailure() {}
+
+            public void onWriteSuccess() {}
+
+            public void onWriteFailure() {}
+
+            private void checkConnection(){
+                if (!client.getClient().isConnected()){
+                    Toaster("You are not connected to the google client, attempting a connection now...");
+                    client.getClient().connect();
+                }
+            }
     }
 
     //Build instructions
--- app/src/main/java/com/z/api/zapi/MainActivity.java
+++ app/src/main/java/com/z/api/zapi/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.common.data.Freezable;
+import com.google.android.gms.drive.Drive;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.PlaceLikelihood;
 
@@ -15,7 +16,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     ZAPIClient zClient;
-    PlacesZAPI zPlaces;
+    DriveZAPI zDrive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,24 +24,54 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         zClient = new ZAPIClient();
-        zPlaces = new PlacesZAPI();
+        zDrive = new DriveZAPI();
 
-        zClient.AddAPI(zPlaces);
+        zClient.AddAPI(zDrive);
         zClient.registerClient(this,this);
         //final Context c = this;
 
+        //Run's code when the zClient connects
         zClient.addCallback(zClient.new onCreateCallback(){
             @Override
             public void onCreate() {
 
+                //Create a new GoogleDocument
+                DriveZAPI.GoogleDocument myDoc = zDrive.new GoogleDocument(zClient){
 
-                PlacesZAPI.placesRequest pRequest = zPlaces.new placesRequest(zClient, getParent()){
+                    //Sets up code to be run when the document is successfully generated
                     @Override
-                    public void onReturn(ArrayList<PlaceLikelihood> places) {
-                        System.out.println(places.get(1).getPlace().getAddress());
+                    public void onGenerateSuccess(String id) {
+                        content = "Some content here";
+                        this.Write();
+                    }
+
+                    //Sets up code to be run when the document is successfully written to
+                    @Override
+                    public void onWriteSuccess() {
+                        System.out.println("Made a file with id: " + getID());
                     }
                 };
+
+                //Generates document
+                myDoc.generateDocument("My File",true);
             }
         });
+    }
+
+    //Example of how to write to a known id
+    public void writeToId(String id, String content){
+        DriveZAPI.GoogleDocument someDocument = zDrive.new GoogleDocument(zClient); //create a new class to represent the document
+        someDocument.content = content; //add content to the document
+        someDocument.WriteToId(id);
+    }
+
+    //Example of how to load from a known id asynchronously
+    public String loadFromId(String id){
+        DriveZAPI.GoogleDocument someDocument = zDrive.new GoogleDocument(zClient);
+
+        someDocument.LoadFromId(id);
+        while (!someDocument.hasLoaded);
+
+        return someDocument.content;
     }
 }
